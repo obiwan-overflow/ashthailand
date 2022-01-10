eff69e645bf62a80fa7a23a402b4e8ae9bdc428e
obiwan-overflow <kittipopp@hotmail.co.th>
2022-01-10 17:03:02 +0700

    'update'

--- src/app/signin/signin.page.ts
+++ src/app/signin/signin.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { RestApiService } from '../rest-api.service';
+import { Storage } from '@ionic/storage-angular';
+import { AlertController, LoadingController, Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-signin',
@@ -7,13 +10,39 @@ import { Router } from '@angular/router';
   styleUrls: ['./signin.page.scss'],
 })
 export class SigninPage implements OnInit {
-
-  constructor(public router: Router) { }
+  formlogin:Formlogin;
+  constructor(public route: Router,public api:RestApiService,private storage: Storage,public loadingController:LoadingController) { }
 
   ngOnInit() {
+    this.formlogin = new Formlogin();
   }
   async signin(){
-    this.router.navigateByUrl('home-new');
+    const loading = await this.loadingController.create({
+      cssClass: 'my-custom-class',
+      message: 'Please wait...',
+      duration: 2000
+    });
+    await this.api.getdata('member/login&username='+this.formlogin.username+'&password='+this.formlogin.password).subscribe(res=>{
+      if(res.result == 'success'){
+        this.storage.set('userId',res.detail.id);
+        this.route.navigateByUrl('home-new');
+        loading.present();
+      }else{
+        this.loginfailed();
+      }
+    });
+  }
+  async loginfailed(){
+    const loading = await this.loadingController.create({
+      cssClass: 'my-custom-class',
+      message: 'Failed !',
+      duration: 2000
+    });
+    loading.present();
   }
 
+}
+class Formlogin {
+  username:any;
+  password:any;
 }
