785499fe365abb7c324f30d5329fd1e128f130da
obiwan-overflow <kittipopp@hotmail.co.th>
2022-01-24 10:34:18 +0700

    update

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
+import { Router } from '@angular/router';
+import { AlertController, LoadingController, Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-tabs',
@@ -6,7 +9,36 @@ import { Component } from '@angular/core';
   styleUrls: ['tabs.page.scss']
 })
 export class TabsPage {
+  fullname:any;
+  constructor(private storage: Storage,public router:Router,public loadingController:LoadingController) {
+    this.storage.get('fullname').then((data)=>{
+      this.fullname = data;
+    });
+  }
 
-  constructor() {}
+  async tabForm(){
+    if(this.fullname == undefined){
+      const loading = await this.loadingController.create({
+        cssClass: 'my-custom-class',
+        message: 'กรุณาเข้าสู่ระบบ ...',
+        duration: 1000
+      });
+      loading.present();
+    }else{
+      this.router.navigateByUrl('tabs/form');
+    }
+  }
+  async tabReport(){
+    if(this.fullname == undefined){
+      const loading = await this.loadingController.create({
+        cssClass: 'my-custom-class',
+        message: 'กรุณาเข้าสู่ระบบ ...',
+        duration: 1000
+      });
+      loading.present();
+    }else{
+      this.router.navigateByUrl('tabs/report');
+    }
+  }
 
 }
