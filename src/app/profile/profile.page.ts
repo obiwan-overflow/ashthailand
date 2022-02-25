360437d846b78d482550846894682769f32bada6
obiwan-overflow <kittipopp@hotmail.co.th>
2022-02-25 19:22:50 +0700

    update

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -29,6 +29,7 @@ export class ProfilePage implements OnInit {
   // loading 
   loading:any;
   loadingImg:any;
+  saveloading:any;
 
   // images
   profileImg:any;
@@ -39,14 +40,6 @@ export class ProfilePage implements OnInit {
   compareWithFn(o1, o2) {
     return o1 === o2;
   };
-  gelleryOptions: CameraOptions = {
-    quality: 50,
-    sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
-    destinationType: this.camera.DestinationType.DATA_URL,
-    encodingType: this.camera.EncodingType.JPEG,
-    mediaType: this.camera.MediaType.PICTURE,
-    allowEdit: false
-  }
   constructor(
     public api:RestApiService,
     public storage:Storage,
@@ -65,6 +58,41 @@ export class ProfilePage implements OnInit {
   async ionViewWillEnter(){
     await this.presentLoading();
     // await this.loadDataselect();
+  }
+  async presentLoading() {
+    this.loading = await this.loadingController.create({
+      cssClass: 'my-custom-class',
+      message: 'กรุณารอสักครู่...',
+    });
+    this.loading.present();
+
+    await this.storage.get('userData').then((data)=>{
+      this.name               = data.name;
+      this.lastname           = data.lastname;
+      this.organization_name  = data.organization_name;
+      this.province           = data.province;
+      this.district           = data.district;
+      this.subdistrict        = data.subdistrict;
+      this.phone              = data.phone;
+      this.id                 = data.id;
+      this.image              = data.image == " " ? 'assets/images/user-theme.png' : data.image;
+    });
+    await this.api.getdata('member/getProvincesList&id_province='+this.province+'&id_amphures='+this.district+'&id_tombons='+this.subdistrict).subscribe((res)=>{
+      this.detailProvince = res.detail;
+      this.stopLoading();
+    });
+  }
+  async stopLoading(){
+    await this.loading.dismiss();
+    console.log('Loading dismissed!');
+  }
+
+  // images
+  gelleryOptions: CameraOptions = {
+    quality: 50,
+    sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    allowEdit: false
   }
   async openGallery() {
     this.loadingImage();
@@ -87,6 +115,9 @@ export class ProfilePage implements OnInit {
     this.image = images;
     this.loadingImg.dismiss();
   }
+
+
+  // form
   async form(){
     const alert = await this.alertController.create({
       cssClass: 'my-custom-class',
@@ -117,6 +148,7 @@ export class ProfilePage implements OnInit {
             formData.append('image',this.image);
             this.api.postdata('member/editProfile',formData).subscribe((res)=>{
               if(res.result == 'success'){
+                this.uploadLoading();
                 this.setUserData(res.detail);
               }
             });
@@ -126,10 +158,19 @@ export class ProfilePage implements OnInit {
     });
     await alert.present();
   }
+  async uploadLoading() {
+    this.saveloading = await this.loadingController.create({
+      cssClass: 'my-custom-class',
+      message: 'กำลังบันทึกข้อมูล...',
+    });
+    this.saveloading.present();
+  }
+
   async setUserData(data){
     await this.storage.remove('userData');
     await this.storage.set('userData',data);
-    this.presentToast();
+    await this.saveloading.dismiss();
+    await this.presentToast();
   }
   async presentToast() {
     const toast = await this.toastController.create({
@@ -139,33 +180,6 @@ export class ProfilePage implements OnInit {
       position:"top"
     });
     toast.present();
-  }
-  async presentLoading() {
-    this.loading = await this.loadingController.create({
-      cssClass: 'my-custom-class',
-      message: 'กรุณารอสักครู่...',
-    });
-    this.loading.present();
-
-    await this.storage.get('userData').then((data)=>{
-      this.name               = data.name;
-      this.lastname           = data.lastname;
-      this.organization_name  = data.organization_name;
-      this.province           = data.province;
-      this.district           = data.district;
-      this.subdistrict        = data.subdistrict;
-      this.phone              = data.phone;
-      this.id                 = data.id;
-      this.image              = data.image == " " ? 'assets/images/user-theme.png' : data.image;
-    });
-    await this.api.getdata('member/getProvincesList&id_province='+this.province+'&id_amphures='+this.district+'&id_tombons='+this.subdistrict).subscribe((res)=>{
-      this.detailProvince = res.detail;
-      this.stopLoading();
-    });
-  }
-  async stopLoading(){
-    await this.loading.dismiss();
-    console.log('Loading dismissed!');
   }
   async loadDataselect(){
     await this.api.getdata('member/getProvinces').subscribe((res)=>{
