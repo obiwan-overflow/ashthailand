b3e184724d3127d0ed70c4044123ac0c070d1c48
obiwan-overflow <kittipopp@hotmail.co.th>
2022-03-17 14:56:33 +0700

    update

--- src/app/formthree/form-draft/form-draft.page.ts
+++ src/app/formthree/form-draft/form-draft.page.ts
@@ -15,12 +15,25 @@ export class FormDraftPage implements OnInit {
   async ionViewWillEnter(){
     let dataStorage = [];
     dataStorage = await this.storage.get('formfamily');
-    this.data = dataStorage;
- 
+
     // for (let val of dataStorage){
     //   if(val.MOO === val.MOO && val.VIL === val.VIL && val.A1 === val.A1){
     //     this.data.push(val);
     //   }
     // }
+
+    var helper = {};
+    var result = dataStorage.reduce(function(r, o) {
+      var key = o.MOO + '-' + o.VIL + '-' + o.A1;
+      
+      if(!helper[key]) {
+        helper[key] = Object.assign({}, o); // create a copy of o
+        r.push(helper[key]);
+      }
+
+      return r;
+    }, []);
+    this.data = result;
+    
   }
 }
