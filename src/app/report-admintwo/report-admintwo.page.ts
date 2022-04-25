f328d2d2a2e5c8b26cc85bfd15046edfffd967b0
obiwan-overflow <kittipopp@hotmail.co.th>
2022-04-25 17:19:13 +0700

    update

--- src/app/report-admintwo/report-admintwo.page.ts
+++ src/app/report-admintwo/report-admintwo.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RestApiService } from '../rest-api.service';
 import { Storage } from '@ionic/storage-angular';
 import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-report-admintwo',
@@ -12,10 +13,12 @@ export class ReportAdmintwoPage implements OnInit {
   private todo : FormGroup;
   user:any = {};
   formData:boolean;
+  id:any;
   constructor(
     public api:RestApiService,
     public storage:Storage,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private route:ActivatedRoute
   ) {
     this.todo = this.formBuilder.group({
       dateStart: ['', Validators.required],
@@ -30,6 +33,8 @@ export class ReportAdmintwoPage implements OnInit {
     await this.storage.get('userData').then((data)=>{
       this.user = data;
     });
+    this.id = this.route.snapshot.paramMap.get('id');
+    
   }
   async logForm(){
     this.formData = true;
