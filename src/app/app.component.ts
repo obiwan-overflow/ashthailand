d56e07189de987b76c28b819351554deb71d6492
obiwan-overflow <kittipopp@hotmail.co.th>
2022-06-01 17:20:11 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,7 +26,9 @@ export class AppComponent {
       // this.alertNetwork();
     });
     document.addEventListener("online",()=>{
-      this.updateData();
+      this.updateDataPublic();
+      this.updateDataShop();
+      this.updateDataFamily();
     });
   }
   async ngOnInit() {
@@ -65,42 +67,137 @@ export class AppComponent {
     await alert.present();
   }
 
-  async updateData(){
-    let dataPublic = await this.storage.get('formPublicFailed');
-    for (let index = 0; index < dataPublic.length; index++) {
-      const element = dataPublic[index];
-
-      // const formData = new FormData();
-      // formData.append('cat_id',"1");
-      // formData.append('user_id',element.userId);
-      // formData.append('organization_name',element.organization_name);
-      // formData.append('CWT',element.CWT);
-      // formData.append('TMP',element.TMP);
-      // formData.append('ID1',element.ID1);
-      // formData.append('VIL',element.VIL);
-      // formData.append('MOO',element.MOO);
-      // formData.append('A1',element.A1);
-      // formData.append('NAME',element.NAME);
-      // formData.append('ADDRESS',element.ADDRESS);
-      // formData.append('LAT',element.LAT);
-      // formData.append('LONG',element.LONG);
-      // formData.append('P1A',element.P1A);
-      // formData.append('P2A',element.P2A);
-      // formData.append('P3A',element.P3A);
-      // formData.append('P4A',element.P4A);
-      // formData.append('P5A',element.P5A == undefined ? "" : element.P5A);
-      // this.api.postdata('reportQuestion',formData).subscribe((res)=>{
-      //   if(res.result == 'success'){
-      //     this.deleteData(index);
-      //   }
-      // },(err)=>{
-      //   alert(err);
-      // });
+  async updateDataPublic(){
+    let dataPublic  = await this.storage.get('formPublicFailed');
+    if(dataPublic !== undefined || dataPublic !== null || dataPublic !== ""){
+      const formData = new FormData();
+      formData.append('cat_id',"1");
+      formData.append('user_id',dataPublic[0].userId);
+      formData.append('organization_name',dataPublic[0].organization_name);
+      formData.append('CWT',dataPublic[0].CWT);
+      formData.append('TMP',dataPublic[0].TMP);
+      formData.append('ID1',dataPublic[0].ID1);
+      formData.append('VIL',dataPublic[0].VIL);
+      formData.append('MOO',dataPublic[0].MOO);
+      formData.append('A1',dataPublic[0].A1);
+      formData.append('NAME',dataPublic[0].NAME);
+      formData.append('ADDRESS',dataPublic[0].ADDRESS);
+      formData.append('LAT',dataPublic[0].LAT);
+      formData.append('LONG',dataPublic[0].LONG);
+      formData.append('date_start',dataPublic[0].dateStart);
+      formData.append('date_success',dataPublic[0].dateSuccess);
+      formData.append('P1A',dataPublic[0].P1A);
+      formData.append('P2A',dataPublic[0].P2A);
+      formData.append('P3A',dataPublic[0].P3A);
+      formData.append('P4A',dataPublic[0].P4A);
+      formData.append('P5A',dataPublic[0].P5A == undefined ? "" : dataPublic[0].P5A);
+      this.api.postdata('reportQuestion',formData).subscribe((res)=>{
+        if(res.result == 'success'){
+          this.deleteDataPublic();
+        }
+      },(err)=>{
+        alert('การอัพโหลดข้อมูลมีปัญหา');
+      });
     }
   }
-  async deleteData(index){
-    let dataPublic = await this.storage.get('formPublicFailed');
-    dataPublic.splice(index,1);
-    this.storage.set('formpublic',dataPublic);
+  async updateDataShop(){
+    let dataShop    = await this.storage.get('formShopFailed');
+    if(dataShop !== undefined || dataShop !== null || dataShop !== ""){
+      const formData = new FormData();
+      formData.append('cat_id',"2");
+      formData.append('user_id',dataShop[0].userId);
+      formData.append('organization_name',dataShop[0].organization_name);
+      formData.append('CWT',dataShop[0].CWT);
+      formData.append('TMP',dataShop[0].TMP);
+      formData.append('ID1',dataShop[0].ID1);
+      formData.append('VIL',dataShop[0].VIL);
+      formData.append('MOO',dataShop[0].MOO);
+      formData.append('A1',dataShop[0].A1);
+      formData.append('NAME',dataShop[0].NAME);
+      formData.append('ADDRESS',dataShop[0].ADDRESS);
+      formData.append('LAT',dataShop[0].LAT);
+      formData.append('LONG',dataShop[0].LONG);
+      formData.append('date_start',dataShop[0].dateStart);
+      formData.append('date_success',dataShop[0].dateSuccess);
+      formData.append('S1A',dataShop[0].S1A);
+      formData.append('S2A',dataShop[0].S2A);
+      formData.append('S3A',dataShop[0].S3A);
+      formData.append('S4A',dataShop[0].S4A);
+      formData.append('S5A',dataShop[0].S5A);
+      formData.append('S6A',dataShop[0].S6A);
+      this.api.postdata('reportQuestion',formData).subscribe((res)=>{
+        if(res.result == 'success'){
+          this.deleteDataShop();
+        }
+      },(err)=>{
+        alert('การอัพโหลดข้อมูลมีปัญหา');
+      });
+    }
+  }
+  async updateDataFamily(){
+    let dataFamily  = await this.storage.get('formFamilyFailed');
+    if(dataFamily !== undefined || dataFamily !== null || dataFamily !== ""){
+      const formData = new FormData();
+      formData.append('cat_id',"3");
+      formData.append('user_id',dataFamily[0].userId);
+      formData.append('organization_name',dataFamily[0].organization_name);
+      formData.append('MEMBER',dataFamily[0].MEMBER);
+      formData.append('PERSON_NO',dataFamily[0].PERSON_NO);
+      formData.append('SEX',dataFamily[0].SEX);
+      formData.append('AGE',dataFamily[0].AGE);
+      formData.append('CWT',dataFamily[0].CWT);
+      formData.append('TMP',dataFamily[0].TMP);
+      formData.append('ID1',dataFamily[0].ID1);
+      formData.append('VIL',dataFamily[0].VIL);
+      formData.append('MOO',dataFamily[0].MOO);
+      formData.append('A1',dataFamily[0].A1);
+      formData.append('NAME',dataFamily[0].NAME);
+      formData.append('ADDRESS',dataFamily[0].ADDRESS);
+      formData.append('LAT',dataFamily[0].LAT);
+      formData.append('LONG',dataFamily[0].LONG);
+      formData.append('date_start',dataFamily[0].dateStart);
+      formData.append('date_success',dataFamily[0].dateSuccess);
+      formData.append('SMOKE',dataFamily[0].SMOKE);
+      formData.append('EVERSMOKE',dataFamily[0].EVERSMOKE);
+      formData.append('EXSMOKE_Y',dataFamily[0].EXSMOKE_Y);
+      formData.append('EXSMOKE_M ',dataFamily[0].EXSMOKE_M);
+      formData.append('TIME_Y',dataFamily[0].TIME_Y);
+      formData.append('TIME_M',dataFamily[0].TIME_M);
+      formData.append('CIG',dataFamily[0].CIG);
+      formData.append('ROLL',dataFamily[0].ROLL);
+      formData.append('E_CIG',dataFamily[0].E_CIG);
+      formData.append('OTHER',dataFamily[0].OTHER);
+      formData.append('NO1',dataFamily[0].NO1);
+      formData.append('NO2',dataFamily[0].NO2);
+      formData.append('RESPONSE',dataFamily[0].RESPONSE);
+      formData.append('TYPE_CIG',dataFamily[0].TYPE_CIG);
+      formData.append('SECOND',dataFamily[0].SECOND);
+      formData.append('QUITE_CHECK',dataFamily[0].QUITE_CHECK);
+      this.api.postdata('reportQuestion',formData).subscribe((res)=>{
+        if(res.result == 'success'){
+          this.deleteDataFamily();
+        }
+      },(err)=>{
+        alert('การอัพโหลดข้อมูลมีปัญหา');
+      });
+    }
+  }
+  async deleteDataPublic(){
+    let dataPublic  = await this.storage.get('formPublicFailed');
+    await dataPublic.splice(0,1);
+    await this.storage.set('formPublicFailed',dataPublic);
+    await this.updateDataPublic();
+  }
+  async deleteDataShop(){
+    let dataShop  = await this.storage.get('formShopFailed');
+    await dataShop.splice(0,1);
+    await this.storage.set('formShopFailed',dataShop);
+    await this.updateDataShop();
+  }
+  async deleteDataFamily(){
+    let dataFamily  = await this.storage.get('formFamilyFailed');
+    await dataFamily.splice(0,1);
+    await this.storage.set('formFamilyFailed',dataFamily);
+    await this.updateDataFamily();
   }
 }
